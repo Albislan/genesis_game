eda609bdb2c1d3ce9bf142048fc240a1b7f32eac
Albislan <albislan@hotmail.com>
2020-11-25 10:49:12 -0300

    Aprimorando o Jogo e implementando alguns sons para animar o jogo

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let order = [];
 let clickedOrder = [];
 let score = 0;
-
+var soundBlue=document.getElementById("t_blue");
+var soundRed=document.getElementById("t_red");
+var soundGreen=document.getElementById("t_green");
+var soundYellow=document.getElementById("t_yellow");
+var soundGameOver=document.getElementById("t_gameover");
+var soundOK=document.getElementById("t_acertou");
 //0 - verde
 //1 - vermelho
 //2- amarelo
@@ -42,7 +47,8 @@ let checkOrder = () => {
         }
     }
     if(clickedOrder.length == order.length) {
-        alert('Pontuação: ${score}\nVoçê acertou! Iniciando próximo nível');
+        soundOK.play()
+        alert('Pontuação: ' + score + '\n Muito Bom \n Voçê acertou! \n Iniciando próximo nível');
         nextLevel();
 
     }
@@ -62,15 +68,19 @@ let click = (color) => {
 
 let createColorElement = (color) => {
     if(color == 0) {
+        soundGreen.play()
         return green;
 
     } else if(color == 1) {
+        soundRed.play()
         return red;
 
     } else if(color == 2) {
+        soundYellow.play()
         return yellow;
 
     } else if(color == 3) {
+        soundBlue.play()
         return blue
     }
 }
@@ -82,7 +92,8 @@ let nextLevel = () => {
 
 
 let gameOver = () => {
-    alert('Pontuação: ${score}!\nVocê perdeu o Jogo!\nClique em OK para iniciar um novo jogo');
+    soundGameOver.play()
+    alert('Pontuação: ' + score + '\n Que Peninha! \n Você perdeu o Jogo!\nClique em OK para iniciar um novo jogo');
     order = [];
     clickedOrder = [];
 
@@ -90,7 +101,8 @@ let gameOver = () => {
 }
 
 let playGame = () => {
-    alert('Bem Vindo ao Genesis! Iniciando novo Jogo!');
+    soundGameOver.pause()
+    alert('Bem Vindo ao Genesis! \n Iniciando novo Jogo!');
     score = 0;
 
     nextLevel();
